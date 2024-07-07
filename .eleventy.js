a5de850f3f88554900523a0c224f6ed0964513e9
JamFox <github@jamfox.dev>
2024-07-07 17:37:29 +0300

    add cname file

--- .eleventy.js
+++ .eleventy.js
@@ -119,6 +119,7 @@ module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy({'./src/assets/img': './assets/img'})
     eleventyConfig.addPassthroughCopy({'./src/assets/fonts': './assets/fonts'})
     eleventyConfig.addPassthroughCopy({'./src/assets/ico': './'})
+    eleventyConfig.addPassthroughCopy({'./src/assets/CNAME': './'})
 
     // Markdown ----------------------------------------
 
